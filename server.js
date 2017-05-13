8fbcc48cc6e129095c4dc7cddea6bcf69555dc6c
akashraj11 <akashraj0134@gmail.com>
2017-05-13 23:15:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var article={
+var articles={
  'article-One' : {    
     title:'Article One! Akashraj',
     heading:'Article One',
