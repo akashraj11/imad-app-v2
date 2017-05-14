764c5ae6746fd14bd2002e5f1f275fb1e31c261d
akashraj11 <akashraj0134@gmail.com>
2017-05-14 11:24:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name/:name', function (req, res) {
     //get the name from request
-    var name = req.params.articleName;
+    var name = req.params.name;
     names.push(name);
     //JSON
   res.send(JSON.stringify(names));
