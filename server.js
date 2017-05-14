c85f3a7f16ab2ef83099e5dafe5c1d5456067b25
akashraj11 <akashraj0134@gmail.com>
2017-05-14 09:44:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ var counter = 0;
 app.get('/counter',function(req,res) {
     counter = counter + 1;
     res.send(counter,toString());
-}
+});
 
 
 app.get('/ui/style.css', function (req, res) {
