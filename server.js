e3aedc9fa69542cd995bae995d79fb87e75cd75f
akashraj11 <akashraj0134@gmail.com>
2017-05-15 20:49:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-    user : 'posgres',
+    user : 'postgres',
     database: 'postgres',
     host: 'db.imad.hasura-app.io',
     port: '5432',
